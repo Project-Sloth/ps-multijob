2b37c81d0e37d13e66bd364f9954188044ce6098
Christian <Futte8D@hotmail.com>
2022-09-16 16:50:48 +0200

    Added components

--- svelte-source/src/App.svelte
+++ svelte-source/src/App.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
-  import { EventHandler } from './utils/eventHandler';
+	import CategoryMenu from './components/CategoryMenu.svelte';
+	import NavBar from './components/NavBar.svelte';
+	import { EventHandler } from './utils/eventHandler';
 
-  EventHandler();
+	EventHandler();
+
+	let navItemSelected: boolean = true;
+	let activeNavItem: string;
 </script>
 
-<main class="min-h-screen">
+<main class="min-h-screen flex justify-end">
+	{#if navItemSelected}
+		<CategoryMenu {activeNavItem} />
+	{/if}
+	<NavBar bind:navItemSelected bind:activeNavItem />
 </main>
+
+<style>
+</style>
