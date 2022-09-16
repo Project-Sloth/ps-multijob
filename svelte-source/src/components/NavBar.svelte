81e06fed0390477602e73c1940c50191bf7051d6
Christian <Futte8D@hotmail.com>
2022-09-16 16:50:24 +0200

    Added navigation

---
+++ svelte-source/src/components/NavBar.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { NavItems } from './../configs/nav.config';
+	import NavItem from './atoms/NavItem.svelte';
+
+	export let navItemSelected;
+	export let activeNavItem;
+</script>
+
+<nav class="w-[75px] min-h-screen nav float-right flex flex-col">
+	<div class="ps-logo w-full h-[80px]" />
+	{#each NavItems as item}
+		<NavItem {item} bind:navItemSelected bind:activeNavItem />
+	{/each}
+</nav>
+
+<style>
+	.nav {
+		background: var(--color-darkerblue);
+		border-left: 1px solid var(--color-grey);
+	}
+</style>
---
+++ svelte-source/src/components/atoms/NavItem.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import type { INavItem } from '../../interfaces/INavigation';
+
+	export let item: INavItem;
+	export let navItemSelected: boolean;
+	export let activeNavItem;
+
+	$: activeNavItem = 'Whitelisted';
+
+	function navItemClicked(item: string): void {
+		activeNavItem = item;
+
+		navItemSelected = true;
+	}
+</script>
+
+<div
+	on:click={() => navItemClicked(item.name)}
+	class="navitem w-full h-[60px] flex justify-center items-center cursor-pointer relative"
+	data-item={item.name}
+>
+	<i class="{item.icon} icon" />
+	{#if activeNavItem === item.name}
+		<span class="state w-1 h-full absolute right-0" />
+	{/if}
+</div>
+
+<style lang="scss">
+	.icon {
+		font-size: 26px;
+		color: var(--color-grey);
+	}
+
+	.active {
+		color: var(--color-green);
+		background-color: var(--color-darkestblue);
+	}
+
+	.active-state {
+		background-color: var(--color-green);
+	}
+
+	.navitem:hover {
+		background-color: var(--color-darkestblue);
+	}
+</style>
