9f04a0d7c4d79503553dc751cc52d3ea22f894ec
Christian <Futte8D@hotmail.com>
2022-09-16 02:09:29 +0200

    Added enums

---
+++ svelte-source/src/enums/IconsEnum.ts
@@ -0,0 +1,12 @@
+export enum Icons {
+	Active = 'fa-solid fa-user',
+	BothSelectJob = 'fa-solid fa-check',
+	Civilian = 'fa-brands fa-black-tie',
+	DeleteJob = 'fa-solid fa-trash',
+	Police = 'fa-solid fa-building-shield',
+	Rank = 'fa-regular fa-star',
+	Salary = 'fa-solid fa-dollar-sign',
+	Taxi = 'fa-solid fa-taxi',
+	Whitelist = 'fa-solid fa-check-list',
+	OffDuty = 'fa-solid fa-clock',
+}
---
+++ svelte-source/src/enums/NavEnum.ts
@@ -0,0 +1,4 @@
+export enum NavItemsEnum {
+	Whitelisted = 'Whitelisted',
+	Criminal = 'Criminal',
+}
