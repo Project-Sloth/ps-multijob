35c1ada98ceab9c531a1e4ae93d5ac40c369195b
S1lentcodes <Silentcodes@protonmail.com>
2022-12-08 22:48:07 -0500

    merge jay stuff in and rebuilt

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -39,16 +39,11 @@ local function AddJob(citizenid, job, grade)
     })
 end
 
-local function RemoveJob(citizenid, rjob, rgrade)
+local function RemoveJob(citizenid, job, rgrade)
     local jobs = GetJobs(citizenid)
-    for job, grade in pairs(jobs) do
-        if job == rjob and grade == rgrade then
-            jobs[job] = nil
-            break
-        end
-    end
+    jobs[job] = nil
     local Player = QBCore.Functions.GetPlayerByCitizenId(citizenid)
-    if Player.PlayerData.job.name == rjob then
+    if Player.PlayerData.job.name == job then
         for k,v in pairs(jobs) do
             Player.Functions.SetJob(k,v)
             break
