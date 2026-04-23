5217bd494d8bff7a1e4c62c800e69e12de4c27ea
Phelps <94879374+NotPhelps@users.noreply.github.com>
2026-04-23 03:41:37 -0400

    Update config.lua
    qbox support

--- config.lua
+++ config.lua
@@ -3,58 +3,60 @@ Config = Config or {}
 -- Side of the screen where you want the ui to be on. Can either be "left" or "right"
 Config.Side = "right"
 
+Config.Core = 'qbox'  -- qbox or qb-core
+
 Config.MaxJobs = 3
 Config.IgnoredJobs = {
-	["unemployed"] = true,
+    ["unemployed"] = true,
 }
 
 Config.DenyDuty = {
-	["ambulance"] = true,
-	["police"] = true,
+    ["ambulance"] = true,
+    ["police"] = true,
 }
 
 Config.WhitelistJobs = {
-	["police"] = true,
-	["ambulance"] = true,
-	["mechanic"] = true,
-	["judge"] = true,
-	["lawyer"] = true,
+    ["police"] = true,
+    ["ambulance"] = true,
+    ["mechanic"] = true,
+    ["judge"] = true,
+    ["lawyer"] = true,
 }
 
 Config.Descriptions = {
-	["police"] = "Shoot some criminals or maybe be a good cop and arrest them",
-	["ambulance"] = "Fix the bullet holes",
-	["mechanic"] = "Fix the bullet holes",
-	["tow"] = "Pickup the tow truck and steal some vehicles",
-	["taxi"] = "Pickup people around the city and drive them to their destination",
-	["bus"] = "Pickup multiple people around the city and drive them to their destination",
-	["realestate"] = "Sell houses or something",
-	["cardealer"] = "Sell cars or something",
-	["judge"] = "Decide if people are guilty",
-	["lawyer"] = "Help the good or the bad",
-	["reporter"] = "Lowkey useless",
-	["trucker"] = "Drive a truck",
-	["garbage"] = "Drive a garbage truck",
-	["vineyard"] = "Get them vines",
-	["hotdog"] = "Sell them glizzys",
+    ["police"] = "Shoot some criminals or maybe be a good cop and arrest them",
+    ["ambulance"] = "Fix the bullet holes",
+    ["mechanic"] = "Fix the bullet holes",
+    ["tow"] = "Pickup the tow truck and steal some vehicles",
+    ["taxi"] = "Pickup people around the city and drive them to their destination",
+    ["bus"] = "Pickup multiple people around the city and drive them to their destination",
+    ["realestate"] = "Sell houses or something",
+    ["cardealer"] = "Sell cars or something",
+    ["judge"] = "Decide if people are guilty",
+    ["lawyer"] = "Help the good or the bad",
+    ["reporter"] = "Lowkey useless",
+    ["trucker"] = "Drive a truck",
+    ["garbage"] = "Drive a garbage truck",
+    ["vineyard"] = "Get them vines",
+    ["hotdog"] = "Sell them glizzys",
 }
 
 -- Change the icons to any free font awesome icon, also add other jobs your server might have to the list
 -- List: https://fontawesome.com/search?o=r&s=solid
 Config.FontAwesomeIcons = {
-	["police"] = "fa-solid fa-handcuffs",
-	["ambulance"] = "fa-solid fa-user-doctor",
-	["mechanic"] = "fa-solid fa-wrench",
-	["tow"] = "fa-solid fa-truck-tow",
-	["taxi"] = "fa-solid fa-taxi",
-	["bus"] = "fa-solid fa-bus",
-	["realestate"] = "fa-solid fa-sign-hanging",
-	["cardealer"] = "fa-solid fa-cards",
-	["judge"] = "fa-solid fa-gave",
-	["lawyer"] = "fa-solid fa-gavel",
-	["reporter"] = "fa-solid fa-microphone",
-	["trucker"] = "fa-solid fa-truck-front",
-	["garbage"] = "fa-solid fa-trash-can",
-	["vineyard"] = "fa-solid fa-wine-bottle",
-	["hotdog"] = "fa-solid fa-hotdog",
+    ["police"] = "fa-solid fa-handcuffs",
+    ["ambulance"] = "fa-solid fa-user-doctor",
+    ["mechanic"] = "fa-solid fa-wrench",
+    ["tow"] = "fa-solid fa-truck-tow",
+    ["taxi"] = "fa-solid fa-taxi",
+    ["bus"] = "fa-solid fa-bus",
+    ["realestate"] = "fa-solid fa-sign-hanging",
+    ["cardealer"] = "fa-solid fa-cards",
+    ["judge"] = "fa-solid fa-gave",
+    ["lawyer"] = "fa-solid fa-gavel",
+    ["reporter"] = "fa-solid fa-microphone",
+    ["trucker"] = "fa-solid fa-truck-front",
+    ["garbage"] = "fa-solid fa-trash-can",
+    ["vineyard"] = "fa-solid fa-wine-bottle",
+    ["hotdog"] = "fa-solid fa-hotdog",
 }
